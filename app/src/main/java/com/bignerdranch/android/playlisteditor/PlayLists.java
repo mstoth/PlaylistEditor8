4b86c6afa909992da3912dbd043c260812ff35c3
Michael Toth <michael@virtualpianist.com>
2019-10-30 15:00:39 -0400

    added db

--- app/src/main/java/com/bignerdranch/android/playlisteditor/PlayLists.java
+++ app/src/main/java/com/bignerdranch/android/playlisteditor/PlayLists.java
@@ -5,7 +5,12 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 
 import java.util.ArrayList;
 
@@ -13,20 +18,57 @@ public class PlayLists extends AppCompatActivity {
     private RecyclerView recyclerView;
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager layoutManager;
+    private DBManager dbManager;
+    private SimpleCursorAdapter adapter;
+    final String[] from = new String[] { DatabaseHelper._ID,
+            DatabaseHelper.NAME, DatabaseHelper.JSON };
+    final int[] to = new int[] { R.id.id, R.id.name, R.id.json };
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Intent intent = getIntent();
         ArrayList<String>playLists = intent.getStringArrayListExtra("playlists");
-
         setContentView(R.layout.activity_play_lists);
 
-        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.ZipRecyclerView);
-        MyListAdapter adapter = new MyListAdapter(playLists);
-        recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        recyclerView.setAdapter(adapter);
+        listView = (ListView) findViewById(R.id.list_view);
+
+        // setup database
+        dbManager = new DBManager(this);
+        dbManager.open();
+        Cursor cursor = dbManager.fetch();
+
+        adapter = new SimpleCursorAdapter(this, R.layout.activity_view_record, cursor, from, to, 0);
+        adapter.notifyDataSetChanged();
+
+        listView.setAdapter(adapter);
+
+
+//        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.ZipRecyclerView);
+//        MyListAdapter adapter = new MyListAdapter(playLists);
+//        recyclerView.setHasFixedSize(true);
+//        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+//        recyclerView.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+        if (id == R.id.add_record) {
+            // perform action to add record
+            //Intent add_mem = new Intent(this, AddPlaylist.class);
+            //startActivity(add_mem);
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 
